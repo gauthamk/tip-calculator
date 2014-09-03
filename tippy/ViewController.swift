168a41806dd9a01d3194d10e51a62b1090fb3ba8
Gautham Krishna <gautham.in@gmail.com>
2014-09-02 19:49:47 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Gautham Krishna on 9/2/14.
+//  Copyright (c) 2014 Gautham Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
